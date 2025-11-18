f09a61a0eacdb4151ea3491f560733bb92431fb4
princeton-hyperion <princeton@hypecapital.co>
2025-11-18 11:02:29 +0800

    feat: create index files for components and utilities to streamline exports

---
+++ src/components/ui/index.js
@@ -0,0 +1,6 @@
+export * from "./Button/Button";
+export * from "./ButtonGroup/ButtonGroup";
+export * from "./Input/Input";
+export * from "./InputGroup/InputGroup";
+export * from "./Separator/Separator";
+export * from "./Textarea/Textarea";
