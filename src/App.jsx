fe08c3c77053ecb4c2341f3b5abaf99cc7dec9e1
princeton-hyperion <princeton@hypecapital.co>
2025-11-18 11:06:11 +0800

    feat: implement App component with navigation and authentication buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+import { Button } from "./components";
+import { ROUTES } from "./constants";
+
 export const App = () => {
-  return <></>
-}
+    return (
+        <main className="bg-[#fff9f2] min-h-screen w-full">
+            <header className="fixed top-3 w-full flex justify-center">
+                <div className="flex flex-row border shadow border-gray-700/20 rounded-md justify-between gap-4 items-center py-2 px-4">
+                    <ul className="flex flex-row gap-2">
+                        {ROUTES.map((route) => (
+                            <li key={route.path} className="text-sm">
+                                {route.label}
+                            </li>
+                        ))}
+                    </ul>
+
+                    <div className="flex flex-row gap-2">
+                        <Button variant="outline" size="sm" className="border-gray-700/20 font-normal">
+                            Login
+                        </Button>
+                        <Button size="sm" className="bg-[#294b49] text-white font-normal">
+                            Sign up
+                        </Button>
+                    </div>
+                </div>
+            </header>
+        </main>
+    );
+};
---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const ROUTES = [
+    { label: "Home", path: "home" },
+    { label: "features", path: "features" },
+    { label: "Pricing", path: "pricing" },
+    { label: "FAQ", path: "faq" },
+    { label: "Contact", path: "contact" },
+];
