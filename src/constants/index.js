48aa0fa5d4deba3f8c61cba1d42549f771eb473e
princeton-hyperion <princeton@hypecapital.co>
2025-11-18 13:05:37 +0800

    feat: add additional routes for child care and elderly care services

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,3 +5,10 @@ export const ROUTES = [
     { label: "FAQ", path: "faq" },
     { label: "Contact", path: "contact" },
 ];
+
+export const OTHER_ROUTES = [
+    { label: "Child care", path: "child-care" },
+    { label: "Elderly care", path: "elderly-care" },
+    { label: "General Consultant", path: "general-consultant" },
+    { label: "Injection administration", path: "injection-administration" },
+];
